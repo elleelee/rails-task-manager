90b558d883a8eb74c0e4a5512cca1903773cd9ed
Ellen Lee <ellen811003@gmail.com>
2019-08-13 13:51:27 +0100

    completed task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    check_complete(@task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def check_complete(task)
+    if task.completed == true
+      @task_complete = 'The task is <strong>completed</strong>!'
+    else
+      @task_complete = 'The task is <strong>not completed</strong>!'
+    end
+  end
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
